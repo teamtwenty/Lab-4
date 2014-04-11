ef40de0a380d9a958e6f05f4fcd22f8367affc9c
calope0106 <clopez.1994@yahoo.com>
2014-04-10 22:01:26 -0700

    Update Tree.cpp

--- Lab-4/Lab-4/Tree.cpp
+++ Lab-4/Lab-4/Tree.cpp
@@ -12,21 +12,21 @@
 
 
 
-Tree::Tree()
+Tree::Tree()//constructor
 {
-    root = NULL;
+    root = NULL; 
 }
-Tree::~Tree()
+Tree::~Tree()//free memory (deconstructor)
 {
     
 }
 void Tree::recursivePrintTree(treeNode *node,Print &print)
 {
-    if(node->left!=NULL)
+    if(node->left!=NULL)//check left node
     {
         recursivePrintTree(node->left, print);// clear left node
     }
-    if(node->right!= NULL)
+    if(node->right!= NULL)//check right node
     {
         recursivePrintTree(node->right, print);// Clear right node
     }
@@ -38,12 +38,12 @@ bool Tree::isEmpty()
     return(root==NULL);
 }
 
-void Tree::addNode(Token *newToken, int scannerLineNum)
+void Tree::addNode(Token *newToken, int scannerLineNum)//add a node to the tree
 {
     LinkedList *lineNumber = new LinkedList();
     lineNumber->setLineNum(scannerLineNum);
     
-    if(root == NULL)
+    if(root == NULL)//checks for emptiness so it can put the new node here
     {
         root->data = newToken;
     }
@@ -54,38 +54,38 @@ void Tree::addNode(Token *newToken, int scannerLineNum)
         {
             if(newToken->getTokenString().compare(current->data->getTokenString()))
             {
-                if(current->left != NULL)
+                if(current->left != NULL)//checks left for null
                 {
-                    current = current->left;
+                    current = current->left;//move to left
                     
                 }
                 else
                 {
-                    current->left->data = newToken;
-                    addLineNum(lineNumber, scannerLineNum);
+                    current->left->data = newToken; //fill left node with data
+                    addLineNum(lineNumber, scannerLineNum);//add a lline number
                     current = NULL;
                 }
             }
             else
             {
-                if(current->right != NULL)
+                if(current->right != NULL)//check right for NULL
                 {
-                    current = current->right;
+                    current = current->right;//move to right
                 }
                 else
                 {
-                    current->right->data = newToken;
+                    current->right->data = newToken;//fill right node with data
                     current = NULL;
                 }
             }
         }
     }
 }
-treeNode* Tree::getRoot()
+treeNode* Tree::getRoot() //get the root of the tree
 {
     return this->root;
 }
-void Tree::addLineNum(LinkedList *lineNum,int Num)
+void Tree::addLineNum(LinkedList *lineNum,int Num)//add line number to the tree node
 {
     LinkedList *tmp = root->head;
     while(tmp->getNext() != NULL)
